cda3b3fe35a8589ccea6d908fc6b3e3f59b75370
Brant Burnett <btburnett3@gmail.com>
2009-05-05 20:15:33 -0400

    Initial work on Release 1.10
    Added support for mouse wheel
    Added keypress filtering for numbers, special keys only
    Fixed input always focusing on spinner init (caused error in IE)
    Added event namespacing to improve destroy
    Added allowNull option
    Renamed increment option to step
    Will now load HTML5 min, max, and step attributes
    Now sets the maxlength of the input based on min and max

--- ui.spinner.js
+++ ui.spinner.js
@@ -1,35 +1,70 @@
 (function($) {
 
+var eventNamespace = '.uispinner', // namespace for events on input
+	focusCtrl;	// stores the currently focused spinner
+				// note due to oddities in the focus/blur events, this is part of a two-part system for confirming focus
+				// this must set to the control, and the focus variable must be true
+				// this is because hitting up/down arrows causes focus to change, but blur event for previous control doesn't fire
+
 $.widget("ui.spinner", {
 	_init: function() {
 		var self = this, // shortcut
+			options = self.options,
 			input = self.element,
-			min = self._getData('min'),
-			max = self._getData('max'),
-			maxlength = input.get(0).maxLength,
-			newLimit;
-		
-		if ((input.get(0).tagName != "INPUT") || (input.attr("type") != "text")) {
+			min = options.min,
+			max = options.max,
+			step = options.step,
+			maxlength = -1, temp,
+			type = input.attr("type");
+			
+		if ((input.get(0).tagName != "INPUT") || ((type != "text") && (type != "number"))) {
 			console.error("Invalid target for ui.spinner");
 			return;
 		}
 		
-		// ensure that min is less than or equal to max
-		if ((min != null) && (max != null) && min > max)
-			self._setData('min', min = max);
-
-		// fix min/max based on maxlength of the input
-		if (maxlength) {
-			newLimit = Math.pow(10, maxlength) - 1;
-			if ((max == null) || (max > newLimit))
-				self._setData('max', newLimit);
-			newLimit = -(newLimit + 1) / 10 + 1;
-			if ((min == null) || (min < newLimit))
-				self._setData('min', newLimit);
-		}		
+		// Now parse min, max, and step settings
+		if ((min == null) && ((temp = input.attr("min")) != null))
+			min = parseInt(temp);
+		
+		if ((max == null) && ((temp = input.attr("max")) != null))
+			max = parseInt(temp);
+		
+		if (!step && ((temp = input.attr("step")) != null))
+			if (temp != "any") {
+				step = parseInt(temp);
+				options.largeStep *= step;
+			}
+				
+		if ((max != null) && (min != null)) {
+			// ensure that min is less than or equal to max
+			if (min > max) min = max;
+			
+			// fix min/max based on maxlength of the input
+			maxlength = Math.max(Math.max(maxlength, (max + "").length), (min + "").length);
+		}
+			
+		temp = input.get(0).maxLength;
+		if (temp > 0) {
+			maxlength = maxlength > 0 ? Math.min(temp, maxlength) : temp;
+			temp = Math.pow(10, maxlength) - 1;
+			if ((max == null) || (max > temp))
+				max = temp;
+			temp = -(temp + 1) / 10 + 1;
+			if ((min == null) || (min < temp))
+				min = temp;
+		}
+		
+		if (maxlength > 0)
+			input.attr("maxlength", maxlength);
+					
+		options.min = min;
+		options.max = max;
+		options.step = step || options.defaultStep;
+		
+		// Options are good, now create the spinner
 		
 		self._createButtons(input);
-		self._change(); // process initial value
+		self._change(true); // process initial value, but don't focus
 
 		if (input.get(0).disabled)
 			self.disable();
@@ -74,6 +109,7 @@ $.widget("ui.spinner", {
 			pageDown = keyCode.PAGE_DOWN,
 			home = keyCode.HOME,
 			end = keyCode.END,
+			validKeys = [up, down, right, left, pageUp, pageDown, home, end, keyCode.BACKSPACE, keyCode.DELETE, keyCode.TAB],
 			msie = $.browser.msie;
 			
 		if (className) wrapper.addClass(className);
@@ -85,6 +121,8 @@ $.widget("ui.spinner", {
 		
 		buttons = self.buttons = btnContainer.find('.ui-spinner-button');
 		buttons.css({ width: buttonWidth - (box ? buttons.outerWidth() - buttons.width() : 0), height: height/2 - (box ? buttons.outerHeight() - buttons.height() : 0) });
+		upButton = $(buttons.get(0));
+		downButton = $(buttons.get(1));
 		
 		// fix icon centering
 		icons = buttons.find('.ui-icon');
@@ -93,12 +131,8 @@ $.widget("ui.spinner", {
 		btnContainer.width(buttons.outerWidth());
 
 		showOn = self._getData('showOn');
-		if (showOn == 'focus' || showOn == 'both') // pop-up date picker when in the marked field
-			input.focus(focus).blur(blur);
-		if (showOn == 'hover' || showOn == 'both') {
-			buttons.hover(hoverIn, hoverOut);
-			input.hover(hoverIn, hoverOut);
-		}
+		if (showOn == 'hover' || showOn == 'both')
+			buttons.add(input).bind("mouseenter" + eventNamespace, hoverIn).bind("mouseleave" + eventNamespace, hoverOut);
 		if (showOn != 'always')
 			btnContainer.css('opacity', 0);
 		
@@ -108,13 +142,14 @@ $.widget("ui.spinner", {
 			.mouseout(mouseUp);
 		if (msie)
 			buttons.dblclick(dblClick) // fixes dbl click not firing mouse down
-				.bind("selectstart", function() {return false;}); // select start fixes IE8 dbl click selection
-		upButton = $(buttons.get(0));
-		downButton = $(buttons.get(1));
-		input.keydown(keyDown)
-			.keyup(keyUp)
-			.change(function() { self._change(); })
-			.focus(function() { msie ? selectAll() : setTimeout(selectAll, 0); }); // add delay for Chrome, but breaks IE8
+				.bind("selectstart", function() {return false;}); // fixes IE8 dbl click selection highlight
+		input.bind("keydown" + eventNamespace, keyDown)
+			.bind("keyup" + eventNamespace, keyUp)
+			.bind("change" + eventNamespace, function() { self._change(); })
+			.bind("focus" + eventNamespace, focus)
+			.bind("blur" + eventNamespace, blur);
+		if (options.mouseWheel)
+			$().bind($.browser.mozilla ? "DOMMouseScroll" : "mousewheel", mouseWheel); // bind to document so that mouse doesn't need to be over input
 			
 		function selectAll() { // in a function for compression
 			self.element.select();
@@ -122,25 +157,27 @@ $.widget("ui.spinner", {
 		
 		// events are declared in function so they have access to self variable and better minimization
 		function keyDown(e) {
-			var dir, large, limit;
-			if (keyDir) return false; // only one direction at a time
+			var dir, large, limit,
+				keyCode = e.keyCode; // shortcut for minimization
+			if (e.ctrl || e.alt) return true; // ignore these events
+			if (keyDir || invalidKey(keyCode)) return false; // only one direction at a time, and suppress invalid keys
 			
-			switch (e.keyCode) {
+			switch (keyCode) {
 				case up:
 				case pageUp:
 					dir = 1;
-					large = e.keyCode == pageUp;
+					large = keyCode == pageUp;
 					break;
 					
 				case down:
 				case pageDown:
 					dir = -1;
-					large = e.keyCode == pageDown;
+					large = keyCode == pageDown;
 					break;
 					
 				case right:
 				case left:
-					dir = (e.keyCode == right) ^ rtl ? 1 : -1;
+					dir = (keyCode == right) ^ rtl ? 1 : -1;
 					break;
 					
 				case home:
@@ -154,7 +191,7 @@ $.widget("ui.spinner", {
 					return false;
 			}
 			
-			if (dir) {
+			if (dir) { // only process if dir was set above
 				if (!inKeyDown && !self._getData('disabled')) {
 					keyDir = dir;
 					self._change(); // in case value changed then direction pressed
@@ -173,6 +210,9 @@ $.widget("ui.spinner", {
 		}
 		
 		function keyUp(e) {
+			if (e.ctrl || e.alt) return true; // ignore these events
+			if (invalidKey(e.keyCode)) return false;
+			
 			switch (e.keyCode) {
 				case up:
 				case right:
@@ -186,6 +226,16 @@ $.widget("ui.spinner", {
 					inKeyDown = false;
 					return false;
 			}
+		}
+		
+		function invalidKey(keyCode) {
+			if ((keyCode == 109) // minus sign
+				|| ((keyCode >= 48) && (keyCode <= 57)) // number keys
+				|| ((keyCode >= 96) && (keyCode <= 105))) // numeric keypad
+				return false;
+			for (var i=0; i<validKeys.length; i++) // predefined list of special keys
+				if (validKeys[i] == keyCode) return false;
+			return true;
 		}
 		
 		function setHoverDelay(callback) {
@@ -206,7 +256,7 @@ $.widget("ui.spinner", {
 		function hoverIn() {
 			setHoverDelay(function() {
 				hovered = true;
-				if (!focused)
+				if (!focused || (showOn == 'hover')) // ignore focus flag if show on hover only
 					self.showButtons();
 			});
 		}
@@ -214,20 +264,22 @@ $.widget("ui.spinner", {
 		function hoverOut() {
 			setHoverDelay(function() {
 				hovered = false;
-				if (!focused)
+				if (!focused || (showOn == 'hover')) // ignore focus flag if show on hover only
 					self.hideButtons();
 			});
 		}
 
 		function focus() {
+			msie ? selectAll() : setTimeout(selectAll, 0); // add delay for Chrome, but breaks IE8
 			focused = true;
-			if (!hovered)
+			focusCtrl = self;
+			if (!hovered && (showOn == 'focus' || showOn == 'both')) // hovered will only be set if hover affects show
 				self.showButtons();
 		}
 		
 		function blur() {
 			focused = false;
-			if (!hovered)
+			if (!hovered && (showOn == 'focus' || showOn == 'both')) // hovered will only be set if hover affects show
 				self.hideButtons();
 		}
 		
@@ -240,7 +292,6 @@ $.widget("ui.spinner", {
 			return false;
 		}
 
-		
 		function mouseDown() {
 			if (!options.disabled) {
 				self._change(); // make sure any changes are posted
@@ -270,6 +321,14 @@ $.widget("ui.spinner", {
 			return false;
 		}
 		
+		function mouseWheel(e) { // only bound if options.mousewheel is true
+			if (focused && (focusCtrl === self) && !options.disabled) {
+				self._change(); // make sure changes are posted
+				self._doSpin((e.wheelDelta || -e.detail) > 0 ? 1 : -1);
+				return false;
+			}
+		}
+		
 		function hoverButtonIn() {
 			if (!self._getData('disabled'))
 				$(this).addClass(hover);
@@ -283,7 +342,7 @@ $.widget("ui.spinner", {
 	
 	_spin: function(dir, large) {
 		if (dir > 0)
-			this.increment(large);
+			this.step(large);
 		else
 			this.decrement(large);
 		this.spinCount++;
@@ -316,31 +375,32 @@ $.widget("ui.spinner", {
 	},
 	
 	_doSpin: function(dir, large) {
-		var newValue,
-			options = this.options,
-			value = this.value,
+		var self = this, // shortcut
+			newValue,
+			options = self.options,
+			value = self.value,
 			min = options.min,
 			max = options.max,
-			increment = (large ? options.largeIncrement : options.increment) * dir;
+			step = (large ? options.largeStep : options.step) * dir;
 
 		if (value == null)
 			newValue = (dir > 0 ? min || max : max || min) || 0;
 		else
-			newValue = value + increment;
+			newValue = value + step;
 		if ((max != null) && (newValue > max))
 			newValue = max;
 		else if ((min != null) && (newValue < min))
 			newValue = min;
 		
 		if (newValue != value) {
-			this.value = newValue;
-			this.element.val(newValue).change();
+			self.value = newValue;
+			self.element.val(newValue).change();
 		}
 		
 		return false;
 	},
 	
-	increment: function(large) {
+	step: function(large) {
 		return this._doSpin(1, large);
 	},
 	
@@ -368,6 +428,7 @@ $.widget("ui.spinner", {
 	/* Set the value directly. */
 	setValue: function(value) {
 		this.value = value;
+		this._fixLength(value);
 		this.element.val(value != null ? value: '').change();
 	},
 
@@ -382,18 +443,23 @@ $.widget("ui.spinner", {
 		return value ? parseInt(value) : null;
 	},
 	
-	_change: function() {
-		var value = this._parseValue(),
-			min = this._getData('min'),
-			max = this._getData('max');
+	_change: function(startup) {
+		var self = this, // shortcut
+			value = self._parseValue(),
+			min = self._getData('min'),
+			max = self._getData('max');
+		
+		if ((value == null) && !self._getData('allowNull'))
+			value = self.value != null ? self.value : min || max; // must confirm not null in case just initializing and had blank value
 
 		if (isNaN(value) ||
 			((min != null) && (value < min)) ||
 			((max != null) && (value > max)))
-			value = this.value;
+			value = self.value;
 
-		this.element.val(value).get(0).focus();
-		this.value = value;
+		self.element.val(value != null ? value : "");
+		self.value = value;
+		if (!startup) self.element.focus();
 	},
 	
 	enable: function() {
@@ -410,25 +476,27 @@ $.widget("ui.spinner", {
 	
 	destroy: function(target) {
 		this.wrapper.after(this.element).remove();
-		this.element.css({ width: this._getData('oWidth'), marginRight: this._getData('oMargin') });
+		this.element.unbind(eventNamespace).css({ width: this._getData('oWidth'), marginRight: this._getData('oMargin') });
 		$.widget.prototype.destroy.call(this);
 	}	
 });
 
 $.extend($.ui.spinner, {
-	version: "1.01",
+	version: "1.10",
 	getter: "getValue",
 	defaults: {
 		min: null,
 		max: null,
-		increment: 1,
-		largeIncrement: 10,
+		defaultStep: 1, // real value is "step", and should be passed as such.  Use this value to detect if passed value should override HTML5 attribute
+		largeStep: 10,
 		className: null,
 		showOn: 'always',
 		width: 16,
 		spinDelayOne: 500,
 		spinDelayTwo: 100,
-		spinsBeforeDelayTwo: 3
+		spinsBeforeDelayTwo: 3,
+		mouseWheel: true,
+		allowNull: false
 	}
 });
 
